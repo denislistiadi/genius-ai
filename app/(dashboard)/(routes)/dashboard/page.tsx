48bed1c06fce8b96ad5fc913988d2f0e683cca0a
Denis Listiadi <denislistiadi24@gmail.com>
2023-07-31 14:49:58 +0700

    feat(sidebar): create sidebar

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,9 +1,3 @@
-import { UserButton } from "@clerk/nextjs"
-
 export default function DashboardPage() {
-  return (
-    <div className="flex gap-5">
-      DashboardPage <UserButton afterSignOutUrl="/" />
-    </div>
-  )
+  return <div className="flex gap-5">DashboardPage</div>
 }
