db0f4924fcab592d6e278ab3598f237cb5aef936
Denis Listiadi <denislistiadi24@gmail.com>
2023-08-01 13:15:36 +0700

    feat(code): layout & integrate openai

--- app/api/code/route.ts
+++ app/api/code/route.ts
@@ -1,4 +1,4 @@
-import { Configuration, OpenAIApi } from "openai"
+import { ChatCompletionRequestMessage, Configuration, OpenAIApi } from "openai"
 import { auth } from "@clerk/nextjs"
 
 import { NextResponse } from "next/server"
@@ -8,6 +8,11 @@ const configuration = new Configuration({
 })
 
 const openai = new OpenAIApi(configuration)
+
+const intructionMessage: ChatCompletionRequestMessage = {
+  role: "system",
+  content: "You are a code generator. You must answer only in markdown code snippets. Use code comments for explanations.",
+}
 
 export async function POST(req: Request) {
   try {
@@ -28,13 +33,13 @@ export async function POST(req: Request) {
     }
 
     const response = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages,
+      model: "gpt-3.5-turbo-16k",
+      messages: [intructionMessage, ...messages],
     })
 
     return NextResponse.json(response.data.choices[0].message)
   } catch (error) {
-    console.log("[CONVERSATION_ERROR]", error)
+    console.log("[CODE_ERROR]", error)
     return new NextResponse("Internal error", { status: 500 })
   }
 }
